1ae7904912ccd51e7c6bb8c809140ba11c31637d
vidklopcic <klopcic.vid@gmail.com>
2022-01-25 14:23:39 +0100

    added gitignore

--- glasbena_mladina_anotacije/settings.py
+++ glasbena_mladina_anotacije/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(m9$_b4h+u4kb=gck2dq=ra4tvk0mxr$-y!!4)&%pmd&i@ezz+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', '1') == '1'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
--- glasbena_mladina_anotacije/urls.py
+++ glasbena_mladina_anotacije/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from ajax_select import urls as ajax_select_urls
@@ -20,4 +22,4 @@ from ajax_select import urls as ajax_select_urls
 urlpatterns = [
     path('ajax_select/', include(ajax_select_urls)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
